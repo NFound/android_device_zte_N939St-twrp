a120c9def5210a995c7c86637fd1e3b78cc3e84e
NFound <nfound@yandex.ru>
2016-08-03 18:47:49 +0600

    name

--- full_n939sc.mk
+++ full_n939sc.mk
@@ -34,8 +34,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/dt.img:dt.img
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := n939sc
-PRODUCT_NAME := zx55q05
+PRODUCT_DEVICE := N939sc
+PRODUCT_NAME := zx55q05_64
 PRODUCT_BRAND := ZTE
 PRODUCT_MODEL := Blade X9
 PRODUCT_MANUFACTURER := ZTE
